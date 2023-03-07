8b37c4d353e3f6c1835212e713b89e5caa9fbd84
Luciel <pablisky200@gmail.com>
2023-03-07 18:40:26 -0300

    cambios en formularios, fondo y JS

--- js/app.js
+++ js/app.js
@@ -1,30 +1,34 @@
 //Funciones de ocultar/mostrar sobre mi/experiencia en la barra de navegacion.
 window.onload = () => {
 
-    let x = document.getElementById("quien_soy");
-    let y = document.getElementById("contacto");
-    let z = document.getElementById("experiencia");
+    let quien_soy = document.getElementById("quien_soy");
+    let contacto = document.getElementById("contacto");
+    let experiencia = document.getElementById("experiencia");
 
-    x.style.display = "block";
-    y.style.display = "none";
-    z.style.display = "none";
+    quien_soy.style.display = "block";
+    contacto.style.display = "none";
+    experiencia.style.display = "none";
 };
-function show(param_div_id){
-    let x = document.getElementById("quien_soy");
-    let y = document.getElementById("contacto");
-    let z = document.getElementById("experiencia");
+function show(div_id){
+    window.onload();
     
-    if (param_div_id === "quien_soy"){
-        x.style.display = "block";
-        y.style.display = "none";
-        z.style.display = "none";
-    }else if (param_div_id == "experiencia"){
-        x.style.display = "none";
-        y.style.display = "none";
-        z.style.display = "block";
-    }else{
-        x.style.display = "none";
-        y.style.display = "block";
-        z.style.display = "none";
+    if (div_id === "quien_soy"){
+        quien_soy.style.display = "block";
+        contacto.style.display = "none";
+        experiencia.style.display = "none";
+    }if (div_id === "experiencia"){
+        quien_soy.style.display = "none";
+        contacto.style.display = "none";
+        experiencia.style.display = "block";
+    }if(div_id === "contacto"){
+        quien_soy.style.display = "none";
+        contacto.style.display = "block";
+        experiencia.style.display = "none";
     }
 }
+const form = document.getElementById("contacto_form");
+form.addEventListener("submit", (event) =>{
+    event.preventDefault();
+    let formData = Object.fromEntries( new FormData(event.target));
+    alert("Datos del formulario: "+JSON.stringify(formData));
+});
