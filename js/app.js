394d6d4e13a2dad07aacf29fae581660e7aaa251
Luciel <pablisky200@gmail.com>
2023-03-07 18:56:07 -0300

    agregue comentarios en el JS

--- js/app.js
+++ js/app.js
@@ -1,34 +1,31 @@
-//Funciones de ocultar/mostrar sobre mi/experiencia en la barra de navegacion.
+//Funciones de ocultar/mostrar 
 window.onload = () => {
 
-    let quien_soy = document.getElementById("quien_soy");
-    let contacto = document.getElementById("contacto");
-    let experiencia = document.getElementById("experiencia");
+    let quien_soy = document.getElementById("quien_soy");//Toma el id del div "quien soy".
+    let contacto = document.getElementById("contacto");//Toma el id del div "contacto".
+    let experiencia = document.getElementById("experiencia");//Toma el id del div "ecperiencia".
 
-    quien_soy.style.display = "block";
-    contacto.style.display = "none";
-    experiencia.style.display = "none";
+    quien_soy.style.display = "block";// bloquea el div "quien soy" para que solo se visualice él.
+    contacto.style.display = "none";// oculta el div "contacto".
+    experiencia.style.display = "none";// oculta el div "experiencia".
 };
 function show(div_id){
     window.onload();
     
-    if (div_id === "quien_soy"){
-        quien_soy.style.display = "block";
-        contacto.style.display = "none";
-        experiencia.style.display = "none";
-    }if (div_id === "experiencia"){
-        quien_soy.style.display = "none";
-        contacto.style.display = "none";
-        experiencia.style.display = "block";
+if (div_id === "experiencia"){
+        quien_soy.style.display = "none";// oculta el div "quien soy".
+        contacto.style.display = "none";// oculta el div "contacto".
+        experiencia.style.display = "block";// bloquea el div "experiencia" para que solo se visualice él.
     }if(div_id === "contacto"){
-        quien_soy.style.display = "none";
-        contacto.style.display = "block";
-        experiencia.style.display = "none";
+        quien_soy.style.display = "none";// oculta el div "quien soy".
+        contacto.style.display = "block";// bloquea el div "contacto" para que solo se visualice él.
+        experiencia.style.display = "none";// oculta el div "experiencia".
     }
 }
+//Para cancelar el evento de recargar pagina del sumbit y obtener los datos del formulario.
 const form = document.getElementById("contacto_form");
 form.addEventListener("submit", (event) =>{
-    event.preventDefault();
-    let formData = Object.fromEntries( new FormData(event.target));
-    alert("Datos del formulario: "+JSON.stringify(formData));
+    event.preventDefault();//cancela el evento sumbit.
+    let formData = Object.fromEntries( new FormData(event.target));//en esta variable se toman los datos.
+    alert("Datos del formulario: "+JSON.stringify(formData));//alerta para demostrar que los datos son tomados.
 });
